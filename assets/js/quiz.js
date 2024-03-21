9923cb917ee3dc6f241b468ab047ef9abc8a6e24
mezter1337 <andreas.javert@gmail.com>
2024-03-21 03:15:57 +0100

    Add json.object with questions, choices and audio-placeholders

---
+++ assets/js/quiz.js
@@ -0,0 +1,135 @@
+const quizStructure = [
+    {
+        question: 'Interstellar',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Batman',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Dune',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'The Last Samurai',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Sherlock',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Man of Steel',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Inception',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Pirates',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Rush',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Lion King',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+    {
+        question: 'Gladiator',
+        audio: {
+            url: 'src'
+        },
+        choices: {
+            a: 'choice-one',
+            b: 'choice-two',
+            c: 'choice-three',
+        },
+        correctAnswer: b
+    },
+
+]
