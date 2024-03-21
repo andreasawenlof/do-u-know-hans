55b2e98b878b653051ee018c8241ed8fa963b8ca
mezter1337 <andreas.javert@gmail.com>
2024-03-21 13:29:28 +0100

    Add Enter page to link into the input name page

---
+++ assets/js/enter.js
@@ -0,0 +1,6 @@
+
+function enterPage() {
+    window.location.href = 'name.html';    
+}
+document.getElementById('enterButton').addEventListener('click', enterPage);
+document.getElementById('howTo');
