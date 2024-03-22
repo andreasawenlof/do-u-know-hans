28607d09b36a38b443a6619fa3e1a27cfa50dcb1
mezter1337 <andreas.javert@gmail.com>
2024-03-22 02:53:47 +0100

    Add enter section to play da game

--- assets/js/enter.js
+++ assets/js/enter.js
@@ -2,17 +2,9 @@
 function enterPage() {
     window.location.href = 'name.html';    
 }
-document.getElementById('enterButton').innerHTML = 'ENTER THE GAME';
-
-function howTo() {
-    window.location.href = 'howto.html';
-}
-
-document.getElementById('howTo').innerHTML = 'HOW TO PLAY';
 
 function eventHandler() {
-    document.getElementById('enterButton').addEventListener('click', enterPage);
-    document.getElementById('howTo').addEventListener('click', howTo);
+    document.getElementById('enter-button').addEventListener('click', enterPage);
 }
 
 eventHandler();
