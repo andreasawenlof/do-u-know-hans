ec53f8810165d6c59a1831a478a66b4e9fb85fae
mezter1337 <andreas.javert@gmail.com>
2024-03-23 05:06:18 +0100

    Add comments on name.js

--- assets/js/name.js
+++ assets/js/name.js
@@ -1,12 +1,21 @@
+//Determine variables of name-field and submit-button
 const nameInput = document.getElementById('name');
 const submit = document.getElementById('submit-button');
 
+/**
+ * When submit button is clicked, information in the input field gets store in local storage
+ * Redirects user to the quiz
+ */
 function onSubmitButtonClick() {
     const nameInputSubmit = document.getElementById('name').value;
     localStorage.setItem('name', nameInputSubmit);
     window.location.href = 'quiz.html';
 }
 
+/**
+ * Disables the submit button when the name field is empty.
+ * Submit button gets enabled when the field has information in it
+ */
 function submitButtonDisable() {
     const nameField = document.getElementById('name').value;
     if(nameField !== '') {
@@ -17,8 +26,11 @@ function submitButtonDisable() {
 
 }
 
+//Disable Submit button by default when loading page
 submit.disabled = true;
 
-
+//Event listener triggers the submitButtonDisable function on keyup
 nameInput.addEventListener('keyup', submitButtonDisable);
+
+//Event listener triggers function: onSubmitButtonClick when button gets clicked.
 submit.addEventListener('click', onSubmitButtonClick);
