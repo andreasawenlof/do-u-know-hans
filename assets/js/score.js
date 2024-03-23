662e7d96fd1fb1cb08abd7b9098f9382686dbe15
mezter1337 <andreas.javert@gmail.com>
2024-03-23 05:30:34 +0100

    Add comment in style.css

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,16 +1,32 @@
+/**
+ * Store information from local storage in a variable
+ * Getting score from local storage
+ */
 const correctScoreTotal = Number(localStorage.getItem('correctScore'));
 const incorrectScoreTotal = Number(localStorage.getItem('incorrectScore'));
 
+/**
+ * Use score-variable to input score in html
+ */
 document.getElementById('correctScore').innerHTML = `<span>${correctScoreTotal}</span> Correct <br>Answers `;
 document.getElementById('incorrectScore').innerHTML = `<span>${incorrectScoreTotal}</span> Incorrect <br>Answers`;
 
+/**
+ * Get name from localstorage and stores it in a variable
+ */
 const nameInputSubmit = localStorage.getItem('name');
 document.getElementById('goodJobName').innerHTML = `Well done, ${nameInputSubmit.toUpperCase()}!`;
 
+/**
+ * Function for a Play Again button
+ * Button resets score in local storage
+ * Button also links user to the start of the quiz again
+ */
 function playAgain() {
     localStorage.removeItem('correctScore');
     localStorage.removeItem('incorrectScore');
     window.location.href = 'quiz.html';
 }
 
+// Click event triggers function playAgain
 document.getElementById('play-again').addEventListener('click', playAgain);
