aad264df68c0eeac37fee5f367a625d5d06da795
mezter1337 <andreas.javert@gmail.com>
2024-06-06 09:54:05 +0200

    Fix so user cannot input empty spaces and submittin form

--- assets/js/name.js
+++ assets/js/name.js
@@ -3,7 +3,7 @@ const nameInput = document.getElementById('name');
 const submit = document.getElementById('submit-button');
 
 /**
- * When submit button is clicked, information in the input field gets store in local storage
+ * When submit button is clicked, information in the input field gets stored in local storage
  * Redirects user to the quiz
  */
 function onSubmitButtonClick() {
@@ -13,12 +13,12 @@ function onSubmitButtonClick() {
 }
 
 /**
- * Disables the submit button when the name field is empty.
- * Submit button gets enabled when the field has information in it
+ * DisabDisables the submit button when the name field is empty or contains only spaces.
+ * Submit button gets enabled when the field has information in it.
  */
 function submitButtonDisable() {
-    const nameField = document.getElementById('name').value;
-    if(nameField !== '') {
+    const nameField = document.getElementById('name').value.trim();
+    if (nameField !== '') {
         submit.disabled = false;
     } else if (nameField === '') {
         submit.disabled = true;
@@ -26,11 +26,11 @@ function submitButtonDisable() {
 
 }
 
-//Disable Submit button by default when loading page
+// Disable Submit button by default when loading page
 submit.disabled = true;
 
-//Event listener triggers the submitButtonDisable function on keyup
+// Event listener triggers the submitButtonDisable function on keyup
 nameInput.addEventListener('keyup', submitButtonDisable);
 
-//Event listener triggers function: onSubmitButtonClick when button gets clicked.
+// Event listener triggers function: onSubmitButtonClick when button gets clicked.
 submit.addEventListener('click', onSubmitButtonClick);
